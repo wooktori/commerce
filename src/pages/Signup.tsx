e379381fb0a3cfa9f3e718b697d080b0aead178b
wooktori <jjun9503@gmail.com>
2025-04-07 19:49:24 +0900

    Feat: 회원가입 기능 구현

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -5,23 +5,44 @@ import {
   FormField,
   FormItem,
   FormLabel,
+  FormMessage,
 } from "../components/ui/form";
 import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
+import { z } from "zod";
+import { RadioGroupItem, RadioGroup } from "@/components/ui/radio-group";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-interface IForm {
-  email: string;
-  password: string;
-}
+const formSchema = z
+  .object({
+    nickname: z
+      .string()
+      .min(2, { message: "닉네임은 두 글자 이상이어야 합니다." }),
+    email: z.string().email({ message: "이메일 형식이 아닙니다." }),
+    password: z.string().min(6, { message: "비밀번호는 최소 6자리입니다." }),
+    checkPassword: z
+      .string()
+      .min(6, { message: "비밀번호는 최소 6자리입니다." }),
+    isSeller: z.boolean(),
+  })
+  .refine((data) => data.password === data.checkPassword, {
+    message: "비밀번호가 일치하지 않습니다",
+    path: ["checkPassword"],
+  });
 
 export default function Signup() {
-  const form = useForm<IForm>({
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
       password: "",
+      checkPassword: "",
+      nickname: "",
+      isSeller: false,
     },
   });
-  const onValid = (data: IForm) => {
+
+  const onValid = (data: z.infer<typeof formSchema>) => {
     console.log(data);
   };
 
@@ -32,13 +53,27 @@ export default function Signup() {
         <form onSubmit={form.handleSubmit(onValid)} className="space-y-4">
           <FormField
             control={form.control}
+            name="nickname"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>닉네임</FormLabel>
+                <FormControl>
+                  <Input placeholder="닉네임" type="string" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
             name="email"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Email</FormLabel>
+                <FormLabel>이메일</FormLabel>
                 <FormControl>
-                  <Input placeholder="email" type="email" {...field} />
+                  <Input placeholder="이메일" type="email" {...field} />
                 </FormControl>
+                <FormMessage />
               </FormItem>
             )}
           />
@@ -47,9 +82,56 @@ export default function Signup() {
             name="password"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Password</FormLabel>
+                <FormLabel>비밀번호</FormLabel>
                 <FormControl>
-                  <Input placeholder="password" type="password" {...field} />
+                  <Input placeholder="비밀번호" type="password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="checkPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>비밀번호 확인</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="비밀번호 확인"
+                    type="password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="isSeller"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>판매자 여부</FormLabel>
+                <FormControl>
+                  <RadioGroup
+                    onValueChange={(value) => field.onChange(value === "true")}
+                    defaultValue={field.value ? "true" : "false"}
+                    className="flex space-x-4"
+                  >
+                    <FormItem className="flex items-center space-x-2">
+                      <FormControl>
+                        <RadioGroupItem value="true" />
+                      </FormControl>
+                      <FormLabel>예</FormLabel>
+                    </FormItem>
+                    <FormItem className="flex items-center space-x-2">
+                      <FormControl>
+                        <RadioGroupItem value="false" />
+                      </FormControl>
+                      <FormLabel>아니오</FormLabel>
+                    </FormItem>
+                  </RadioGroup>
                 </FormControl>
               </FormItem>
             )}
