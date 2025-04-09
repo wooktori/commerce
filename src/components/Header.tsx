e6749f1d144c6b857f7f4443227381a430fe6fb8
wooktori <jjun9503@gmail.com>
2025-04-09 21:52:16 +0900

    Feat: 로그아웃 기능

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,38 @@
+import { auth } from "@/firebase";
+import { useAuth } from "@/hooks/useAuth";
 import { Link } from "react-router";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function Header() {
+  const { userData } = useAuth();
+  const queryClient = useQueryClient();
+  console.log(userData);
+
+  const handleClick = async () => {
+    try {
+      await auth.signOut();
+      queryClient.setQueryData(["authUser"], null);
+      queryClient.removeQueries({ queryKey: ["userData"] });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="flex justify-between mx-10 my-5">
       <div className="flex gap-4">
-        <Link to="/login">로그인</Link>
-        <Link to="/signup">회원가입</Link>
+        {userData ? (
+          <>
+            <span>{userData.nickname}</span>
+            <button onClick={handleClick}>로그아웃</button>
+          </>
+        ) : (
+          <>
+            <Link to="/login">로그인</Link>
+            <Link to="/signup">회원가입</Link>
+          </>
+        )}
+
         <span>주문조회</span>
         <span>장바구니</span>
         <span>마이페이지</span>
